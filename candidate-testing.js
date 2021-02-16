8293098cf7899d27e72c001eedf6e2a6a13431d8
k2the2 <kat.keller@gmail.com>
2021-02-16 02:46:54 +0000

    spacing

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,27 +3,27 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = "";
+let candidateName="";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first Americanwoman in space? ";
-let correctAnswer = "Sally Ride";
-let candidateAnswer = "";
-let questions = [
+let question="Who was the first Americanwoman in space? ";
+let correctAnswer="Sally Ride";
+let candidateAnswer="";
+let questions=[
 "Who was the first American woman in space? ", 
 "True or false: 5000 meters = 5 kilometers. ",
 "(5 + 3)/2 * 10 = ? ",
 "Given the array [8, 'Orbit', 'Trajectory, 45'], what entry is at index 2? " ,
 "What is the minimum crew size for the ISS? "
 ];
-let correctAnswers = [
+let correctAnswers=[
   "Sally Ride",
   "True",
   "40",
   "Trajectory",
   "3"
 ];
-let candidateAnswers = [];
-let numOfCorrectAnswers = 0;
+let candidateAnswers=[];
+let numOfCorrectAnswers=0;
 
 
 function askForName() {
@@ -34,8 +34,7 @@ candidateName = input.question("What is your name? ")
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   //candidateAnswer = input.question(question);
-  for (let i = 0; i <questions.length; i++) {
-    candidateAnswers.push(input.question(questions[i]))
+  for (let i = 0; i <questions.length; i++) {candidateAnswers.push(input.question(questions[i]))
   }
 
 }
