d4fb4350668efa8b387f7a5ca11563dc47840e16
k2the2 <kat.keller@gmail.com>
2021-02-16 02:49:47 +0000

    more spacing

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,33 +8,21 @@ let candidateName="";
 let question="Who was the first Americanwoman in space? ";
 let correctAnswer="Sally Ride";
 let candidateAnswer="";
-let questions=[
-"Who was the first American woman in space? ", 
-"True or false: 5000 meters = 5 kilometers. ",
-"(5 + 3)/2 * 10 = ? ",
-"Given the array [8, 'Orbit', 'Trajectory, 45'], what entry is at index 2? " ,
-"What is the minimum crew size for the ISS? "
-];
-let correctAnswers=[
-  "Sally Ride",
-  "True",
-  "40",
-  "Trajectory",
-  "3"
-];
+let questions=["Who was the first American woman in space? ", "True or false: 5000 meters = 5 kilometers. ","(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory, 45'], what entry is at index 2? " ,"What is the minimum crew size for the ISS? "];
+let correctAnswers=["Sally Ride","True","40","Trajectory","3"];
 let candidateAnswers=[];
 let numOfCorrectAnswers=0;
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question("What is your name? ")
+candidateName=input.question("What is your name? ")
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   //candidateAnswer = input.question(question);
-  for (let i = 0; i <questions.length; i++) {candidateAnswers.push(input.question(questions[i]))
+  for (let i=0; i <questions.length; i++) {candidateAnswers.push(input.question(questions[i]))
   }
 
 }
@@ -44,7 +32,7 @@ function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
 console.log(`\nCandidate Name: ${candidateName}`);
-for(let j = 0; j<questions.length; j++){
+for(let j=0; j<questions.length; j++){
   console.log(`${j+1}) ${questions[j]} 
   Your Answer: ${candidateAnswers[j]}
   Correct Answer: ${correctAnswers[j]}\n`);
@@ -54,7 +42,7 @@ for(let j = 0; j<questions.length; j++){
   }
 }
 
-  let grade = (numOfCorrectAnswers/questions.length)*100;
+  let grade=(numOfCorrectAnswers/questions.length)*100;
   console.log(`\n>>>  Overall Grade: ${grade}% (${numOfCorrectAnswers} of ${questions.length} responses correct)  <<<`);
   if (grade>=80) {
     console.log(">>>  Status: PASSED  <<<");
